e2072eaa926209ab436c01fca78d29bdb241779b
AnnaGH <i22.ghazaryan.anna@etud.ufar.am>
2024-10-22 17:34:32 +0400

    Add project files.

---
+++ Class.work/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program1
+{
+    public static bool IsPalindrome(string input)
+    {
+        input = input.ToLower();
+
+        int start = 0;
+        int end = input.Length - 1;
+
+        while (start < end)
+        {
+            if (input[start] != input[end])
+            {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void Main()
+    {
+        string test = "racecar";
+        Console.WriteLine(IsPalindrome(test));
+    }
+}
---
+++ Class.work/arrayOfLists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ArrayOfLists
+{
+    private List<int>[] arrayOfLists;
+    public ArrayOfLists(int size)
+    {
+        arrayOfLists = new List<int>[size];
+        for (int i = 0; i < size; i++)
+        {
+            arrayOfLists[i] = new List<int>();
+        }
+    }
+
+    public void Add(int index, int item)
+    {
+        arrayOfLists[index].Add(item);
+    }
+
+    public void PrintAll()
+    {
+        for (int i = 0; i < arrayOfLists.Length; i++)
+        {
+            Console.WriteLine($"List at index {i}: {string.Join(", ", arrayOfLists[i])}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayOfLists arrayOfLists = new ArrayOfLists(3);
+
+        arrayOfLists.Add(0, 10);
+        arrayOfLists.Add(0, 20);
+        arrayOfLists.Add(1, 30);
+        arrayOfLists.Add(2, 40);
+
+        arrayOfLists.PrintAll();
+    }
+}
